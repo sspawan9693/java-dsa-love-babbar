a7a813afc0fcfee91e15dad1779395d4fe333d2a
pawan <ss233@gamil.com>
2026-05-11 12:58:51 +0530

    added reverse

---
+++ src/AdvanceProblemofArray/sortanArray.java
@@ -0,0 +1,29 @@
+package AdvanceProblemofArray;
+
+import java.util.Arrays;
+
+public class sortanArray {
+    public static int[] getSorting0sand1s(int[]arr){
+        int n = arr.length;
+        int i = 0;
+        int j = n-1;
+
+        while (i < j){
+            if(arr[i] == 1 && arr[j] ==0){
+                arr[i] = 0;
+                arr[j] = 1;
+            }
+            if (arr[i] == 0){
+                i++;
+            }
+            if (arr[j] == 1){
+                j--;
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int [] arr ={1,0,1,1,0,0,1};
+        System.out.println(Arrays.toString(getSorting0sand1s(arr)));
+    }
+}
