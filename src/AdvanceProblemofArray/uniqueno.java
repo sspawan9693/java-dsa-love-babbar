a025293da0fb8cd2e613790efb0b811ded042bbe
pawan <ss2330743@gmail.com>
2026-05-13 18:17:02 +0530

    feat unique no

---
+++ src/AdvanceProblemofArray/uniqueno.java
@@ -0,0 +1,15 @@
+package AdvanceProblemofArray;
+
+public class uniqueno {
+    public static int FindUniqueElement(int[] num){
+        int xorSum =0;
+        for (int n: num){
+            xorSum = xorSum ^ n;
+        }
+        return xorSum;
+    }
+    public static void main(String[] args) {
+        int [] num ={2,3,5,4,5,3,4};
+        System.out.println(FindUniqueElement(num));
+    }
+}
