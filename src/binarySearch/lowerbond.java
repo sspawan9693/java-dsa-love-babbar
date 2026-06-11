f6d3cc13d1ab9c16e4a61de6ab87724d4bcaa037
pawan <ss2330743@gmail.com>
2026-06-11 09:57:26 +0530

    Binary Search in LowerBond case

---
+++ src/binarySearch/lowerbond.java
@@ -0,0 +1,28 @@
+package binarySearch;
+
+public class lowerbond {
+    public static int GetlowerBond(int[]arr,int target){
+        int n = arr.length;
+        int start = 0;
+        int end  = n-1;
+        int ans = -1;
+
+        while (start <= end){
+            int mid = start+(end-start)/2;
+            if (arr[mid]>= target){
+                ans = mid;
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,30,30,40,45,50};
+        int target = 35;
+        int ans = GetlowerBond(arr,target);
+        System.out.println("Ans" + ans);
+    }
+}
